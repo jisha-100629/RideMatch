97b61a70d4bf3a1d03e85cccb8c8c65813a910be
Jisha <jisha.100629@gmail.com>
2026-07-15 22:58:32 +0530

    Update DriverRepository.java

--- RideMatch/src/main/java/com/example/RideMatch/repository/DriverRepository.java
+++ RideMatch/src/main/java/com/example/RideMatch/repository/DriverRepository.java
@@ -1,5 +1,5 @@
 package com.example.RideMatch.repository;
-
+ 
 import com.example.RideMatch.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
